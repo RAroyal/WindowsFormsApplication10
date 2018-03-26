d2794cc1eb9333e40ea06ced0ea14f607e686f7c
rustemRA <rustem_pimp@mail.ru>
2018-03-26 10:12:31 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(34, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(141, 308);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(200, 36);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(141, 308);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Заполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Заменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(200, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 27);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "\r\n";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(257, 430);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(26, 27);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "\r\n";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.GreenYellow;
+            this.button5.Location = new System.Drawing.Point(308, 430);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 26);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "\r\n";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button6.Location = new System.Drawing.Point(200, 463);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(27, 27);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "\r\n";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkViolet;
+            this.button7.Location = new System.Drawing.Point(257, 463);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(26, 27);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "\r\n";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.HotPink;
+            this.button8.Location = new System.Drawing.Point(308, 462);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(27, 27);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "\r\n";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 430);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 50);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Выберите\r\nцвет фона";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(281, 576);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Автор";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mistral", 20F);
+            this.label2.Location = new System.Drawing.Point(12, 531);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 41);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Тюлемисова Дана";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(281, 576);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Стоп";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Visible = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 621);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Тюлемисова Дана";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        int[] Mas = new int[25];
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(150, 380);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); //1
+            Random ran = new Random();// 2
+            for (int i = 0; i < 25; i++)// 3
+            {
+                Mas[i] = ran.Next(-5, 5);//4
+                listBox1.Items.Add("Mas[" + (i + 1).ToString() +//5
+                "] = " + Mas[i].ToString());//6
+            }
+            this.Size = new Size(300, 550);//7
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear(); //1
+            for (int i = 0; i < 25; i++) //2
+            {
+                if (Mas[i] == 0)//3
+                { Mas[i] = 0; }//4
+                else//5
+                {
+                    if (Mas[i] < 0)//6
+                        Mas[i] = Mas[i] * Mas[i];//7
+                    else//8
+                        Mas[i] = Mas[i] + 7;//9
+                }
+
+                listBox2.Items.Add("Mas[" + Convert.ToString(i + 1) +//10
+                    "] = " + Mas[i].ToString());//11
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DodgerBlue;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.GreenYellow;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkViolet;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.HotPink;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label2.Left < this.Width)
+            { label2.Left += 2; }
+            else
+            { label2.Left = -label2.Width; }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        button10.Visible = true;
+        label2.Visible = true;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button10.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            label2.Location = new Point(0,ran.Next(0,500));
+        }
+    }
+}
+
